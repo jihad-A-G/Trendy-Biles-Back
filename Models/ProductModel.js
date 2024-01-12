bedf5d13dcb6bae3297648452b148286dbaaf27e
AbdallahSalami <AbdallahSalami@users.noreply.github.com>
2024-01-12 16:23:38 +0200

    3rd try

--- Models/ProductModel.js
+++ Models/ProductModel.js
@@ -1,19 +1,20 @@
-import  mongoose from "mongoose";
+import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    productName: { type: String },
-    price: { type: Number },
-    image: { type: String },
-    categoryID: { type: mongoose.Schema.Types.ObjectId, ref: "categories" },
-    newPrice: { type: Number },
-    itsNew: { type: Boolean, default: false },
+  productName: { type: String },
+  price: { type: Number },
+  image: { type: String },
+  categoryID: { type: mongoose.Schema.Types.ObjectId, ref: "categories" },
+  newPrice: { type: Number },
+  itsNew: { type: Boolean, default: false },
 });
 
-productSchema.pre("find", function (next) {  // populate here to appear the  
+productSchema.pre("find", function (next) {
+  // populate here to appear the
   this.populate(["categoryID"]);
   next();
 });
 
-const Product = mongoose.model("products", productSchema);  // the products here is the name for table at data base 
+const Product = mongoose.model("products", productSchema); // the products here is the name for table at data base
 
 module.exports = Product;
