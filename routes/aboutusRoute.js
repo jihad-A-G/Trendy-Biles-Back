926a057a67d13375817e1041f38f149f92ac5c31
jihad-A-G <jihadabdlghani73@gmail.com>
2024-01-15 15:57:40 +0200

    Added aboutus CRUD operations

---
+++ routes/aboutusRoute.js
@@ -0,0 +1,15 @@
+import express from "express"
+import * as aboutusController from '../Controllers/AboutUsController.js'
+import upload from "../config/multer.js"
+const router = express.Router()
+
+//GET about us content 
+router.get('/content',aboutusController.getAboutUsContent)
+//GET about us info or create one if doesn't exists
+router.get('/info',aboutusController.getAboutUsInfo)
+//PUT update about us or create if doesn't exist
+router.put('/',upload.single('logoImage'),aboutusController.EditAboutus)
+
+export default router
+
+
