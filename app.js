dde6f37b7fa24f33d3add13dd0d06d2040935032
jihad-A-G <jihadabdlghani73@gmail.com>
2024-02-01 11:47:11 +0200

    removing token middlware from app.js

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ app.use((req,res,next) => {
     next()
 })
 
-app.use((req,res,next) =>{
-    req.cookies.token ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YTkzYWQyYTYyYTZmMWZlOGU0N2VhOCIsIm5hbWUiOiJhaG1hZCIsInJvbGVzIjoiNjVhNjg3Yjg4ZDU3MzRiMTEyNTNlYTYwIiwiaWF0IjoxNzA2NjA1NDczLCJleHAiOjE3MDY2OTE4NzN9.CI2P_5cJqgPeEDtyoz_T4s4BhScvtKWBOmegOHhB_bc'
-    return next()
-})
+// app.use((req,res,next) =>{
+//     req.cookies.token ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YTkzYWQyYTYyYTZmMWZlOGU0N2VhOCIsIm5hbWUiOiJhaG1hZCIsInJvbGVzIjoiNjVhNjg3Yjg4ZDU3MzRiMTEyNTNlYTYwIiwiaWF0IjoxNzA2NjA1NDczLCJleHAiOjE3MDY2OTE4NzN9.CI2P_5cJqgPeEDtyoz_T4s4BhScvtKWBOmegOHhB_bc'
+//     return next()
+// })
 
 //Routes goes here
 app.use("/api/users",UsersRoutes)
