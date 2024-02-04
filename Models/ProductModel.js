46e7f52e1754fa01b942a69580370735d0f76791
AbdallahSalami <AbdallahSalami@users.noreply.github.com>
2024-02-04 20:19:55 +0200

    Revert "Revert "finshing changes in product""
    This reverts commit ae1043cbdeae0afdeccdc0523b348d08f77242ef.

--- Models/ProductModel.js
+++ Models/ProductModel.js
@@ -1,40 +1,41 @@
-  // Product Model (product.model.js)
-  import mongoose from "mongoose";
-
-  const productSchema = new mongoose.Schema({
-    productName: {
-      type: String,
-        required: true,
-      },
-    details: [
-        {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "productDetails",
-      required: true, 
-      },
-      ],   
-    categories: [
+// Product Model (product.model.js)
+import mongoose from "mongoose";
+import Notification from "./notificationModel.js";
+import io from "../config/socketIo.js";
+const productSchema = new mongoose.Schema({
+  productName: {
+    type: String,
+      required: true,
+    },
+  details: [
       {
-      type: mongoose.Schema.Types.ObjectId,
-        ref: "categories",
-        required: true,
-      },
-    ],
-    brand: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "brands",
-      required: false,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "productDetails",
+    required: true, 
     },
-    description: {
-      type: String,
+    ],   
+  categories: [
+    {
+    type: mongoose.Schema.Types.ObjectId,
+      ref: "categories",
       required: true,
     },
-    status: {
-      type: String,
-      enum: ["Accepted", "Rejected", "Pending", "New"],
-      default: "Accepted",
-      required: true,
-    },
+  ],
+  brand: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "brands",
+    required: false,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Accepted", "Rejected", "Pending", "New"],
+    default: "Accepted",
+    required: true,
+  },
 
   });
 
@@ -57,4 +58,29 @@
 
   const Product = mongoose.model("products", productSchema);
 
-  export default Product;
+  const productChangeStream = Product.watch()
+
+//Event listener for any change
+
+productChangeStream.on('change', async (change) => {
+console.log(change);
+  const operationType = change.operationType;
+  const collectionName = change.ns.coll;
+  // const document = change.fullDocument ? JSON.stringify(change.fullDocument) : null;
+ 
+  // Create a new notification instance
+  const notification = new Notification({
+     title: `${operationType} operation in ${collectionName}`,
+     message: 'See the changes', // Store the document as a string
+     readStatus: false, // Default to unread
+     // You can also set the receiver field here if applicable
+  });
+ 
+
+  io.to('superAdminRoom').emit('notification', notification);
+ 
+
+  await notification.save();
+ });
+
+export default Product;
